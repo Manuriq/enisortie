0715e515d47d259ee866d8c4c71e34aab8b95c1d
Manu <riquelme.manuel@hotmail.com>
2023-02-07 16:30:47 +0100

    add crud participant

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mail')
+            ->add('password', PasswordType::class, [
+                'hash_property_path' => 'password',
+                'mapped' => false,
+                'required' => false
+            ])
+            ->add('nom')
+            ->add('prenom')
+            ->add('pseudo')
+            ->add('telephone')
+            ->add('actif')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
