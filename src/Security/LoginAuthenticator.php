b61d4e2a71291556fb1263c498a25fc8d3e906fc
Athalfrid <aurelien.robin44450@gmail.com>
2023-02-08 15:32:23 +0100

    modification LoginAuthenticator pour connexion modification security.yaml pour access_controle 'ROLE_ADMIN'

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -33,7 +33,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
         return new Passport(
             new UserBadge($mail),
-            new PasswordCredentials($request->request->get('motPasse', '')),
+            new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
             ]
