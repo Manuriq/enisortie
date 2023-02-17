48289cad4e1fcc66679ac2435fe52932fa8f611b
Manu <riquelme.manuel@hotmail.com>
2023-02-17 07:12:46 +0100

    Fix des migrations

--- migrations/Version20230210095259.php
+++ migrations/Version20230210095259.php
@@ -23,11 +23,13 @@ final class Version20230210095259 extends AbstractMigration
         $this->addSql('ALTER TABLE participant ADD campus_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE participant ADD CONSTRAINT FK_D79F6B11AF5D55E1 FOREIGN KEY (campus_id) REFERENCES campus (id)');
         $this->addSql('CREATE INDEX IDX_D79F6B11AF5D55E1 ON participant (campus_id)');
+        $this->addSql('ALTER TABLE participant ADD image VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participant DROP image');
         $this->addSql('ALTER TABLE participant DROP FOREIGN KEY FK_D79F6B11AF5D55E1');
         $this->addSql('DROP INDEX IDX_D79F6B11AF5D55E1 ON participant');
         $this->addSql('ALTER TABLE participant DROP campus_id');
